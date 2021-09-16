8a39026e79816b19260716c606f5be0d6635cf15
david <rd02navc2@gmail.com>
2021-09-16 14:28:58 +0800

    Fetching data from API

--- frontend/src/screens/MyScript/MyScript.js
+++ frontend/src/screens/MyScript/MyScript.js
@@ -10,38 +10,47 @@ import ErrorMessage from "../../components/ErrorMessage";
 //a.
 import { Chart } from "react-google-charts";
 
-const Counter = () => (
-    <div className="container">
-      <div className="chevron chevron-up" />
-      <div className="number">256</div>
-      <div className="chevron chevron-down" />
-    </div>
-);
+const kusUrl = "http://10.3.1.194:8081/api/tutorials";
 
-const DidMount = () => (
-      // const response = await fetch("https://api.exchangeratesapi.io/latest?symbols=USD,GBP,CAD");
-      // const json = await response.json();
-      // const rateCurrencyNames = Object.keys(json.rates);
-      // const rateCurrencyValues = Object.values(json.rates);
-      // const chartData = [["Currency Name", "Currency Rate"]]
-      // for (let i = 0; i < rateCurrencyNames.length; i += 1) {
-      //  chartData.push([rateCurrencyNames[i], rateCurrencyValues[i]])
+
+const DidMount = async () => {
+      const response = await fetch("https://api.exchangeratesapi.io/latest?symbols=USD,GBP,CAD");
+      const json = await response.json();
+      const rateCurrencyNames = Object.keys(json.rates);
+      const rateCurrencyValues = Object.values(json.rates);
+      const chartData = [["Currency Name", "Currency Rate"]]
+      for (let i = 0; i < rateCurrencyNames.length; i += 1) {
+        chartData.push([rateCurrencyNames[i], rateCurrencyValues[i]])
+      }
+      // component.setState({
+      //  dataLoadingStatus: "ready",
+      //  chartData: chartData
       // }
-      DidMount.setState({
-        dataLoadingStatus: "ready",
-        chartData: chartData
-      })
-)
+      // )
+}
 
 
 const MyScript = (didMount) => {
+
+const [userData, setUserData] = useState({});
+
+useEffect(() => {
+  getKusApiUserWithFetch();
+}, []);
+
+const getKusApiUserWithFetch = async () => {
+  const response = await fetch(kusUrl);
+  const jsonData = await response.json();
+  setUserData(jsonData);
+};
 
   return (
     <MainScreen title="MyScript API">
       <div className="MyApp">
           <Chart 
-            chartType="BarChart"
-            data={didMount.state.chartData}
+            chartType="LineChart"
+            // data={component.state.chartData}
+            data={userData}
             options={{
               chartArea: {
                 width:"50%"
